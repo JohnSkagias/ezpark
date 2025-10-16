c19eead49890deaf8252687082aba7334a680c8d
johns <johnskagias77@gmail.com>
2025-10-16 21:19:43 +0300

    deployment fix 5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,12 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error"],
+    log: ["warn", "error"], // optional
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
